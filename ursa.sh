b0cdca2e762e0fba7e6a1391bc259a40f8c0bf27
Nunoy <83507970+NunoyHaxxana@users.noreply.github.com>
2022-12-10 00:23:41 +0700

    Update ursa.sh

--- ursa.sh
+++ ursa.sh
@@ -7,17 +7,19 @@
 	. $HOME/.cargo/env
 	curl https://deb.nodesource.com/setup_16.x | sudo bash
 	sudo apt install curl make clang pkg-config libssl-dev build-essential git jq nodejs -y < "/dev/null"
-	sudo apt install npm 
+	sudo apt install npm protobuf-compiler 
   
   
   echo -e '\n\e[42mPreparing to install Ursa\e[0m\n' && sleep 1
+  mkdir -p $HOME/fleek/
+  cd $HOME/fleek/
   git clone git@github.com:fleek-network/ursa.git
   cd ursa/
   make install
   
   
   echo -e '\n\e[42mCreating a service\e[0m\n' && sleep 1
-  sudo tee /etc/systemd/system/ursa.service > /dev/null << EOF
+sudo tee /etc/systemd/system/ursa.service > /dev/null << EOF
 [Unit]
 Description=Ursa Node
 After=network-online.target
